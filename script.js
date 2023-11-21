e3e3b0893f38e42c3d133c98e093a998f08f8c79
henrydaniels1 <henrydanielchibuzor@gmail.com>
2023-11-21 15:07:36 +0100

    second commit

---
+++ script.js
@@ -0,0 +1,81 @@
+
+var speechRecognition = window.webkitSpeechRecognition
+var recognition = new speechRecognition()
+var output = document.getElementById('output')
+var previousRecording = document.getElementById('previousRecording')
+var content = ''
+var isRecording = false
+var transcripts = JSON.parse(localStorage.getItem('transcripts')) || []
+recognition.continuous = true
+recognition.onstart = function() {
+ output.textContent = "Voice Recognition is On"
+ isRecording = true
+}
+recognition.onspeechend = function() {
+ output.textContent = "No Activity"
+ isRecording = false
+}
+recognition.onerror = function() {
+ output.textContent = "Try Again"
+}
+recognition.onresult = function(event) {
+ var current = event.resultIndex;
+ var transcript = event.results[current][0].transcript
+ content += transcript
+ output.textContent = content
+}
+document.getElementById('startBtn').addEventListener('click', function(event) {
+ if (isRecording) {
+ recognition.stop()
+ content = ''
+ output.textContent = ''
+ isRecording = false
+ } else {
+ recognition.start()
+ isRecording = true
+ }
+})
+document.getElementById('saveBtn').addEventListener('click', function(event) {
+ transcripts.push(content);
+ localStorage.setItem('transcripts', JSON.stringify(transcripts));
+ previousRecording.textContent = content;
+ content = '';
+})
+document.getElementById('copyBtn').addEventListener('click', function(event) {
+ navigator.clipboard.writeText(output.textContent).then(function() {
+ console.log('Content copied to clipboard');
+ }, function(err) {
+ console.error('Failed to copy: ', err);
+ });
+})
+document.getElementById('deleteBtn').addEventListener('click', function(event) {
+ localStorage.removeItem('transcripts');
+ content = ''
+ output.textContent = ''
+ transcripts = []
+})
+window.onload = function() {
+ var previousTranscripts = JSON.parse(localStorage.getItem('transcripts')) || [];
+ if (previousTranscripts.length > 0) {
+ previousRecording.textContent = previousTranscripts[previousTranscripts.length - 1];
+ }
+ previousRecording.addEventListener('click', function(event) {
+ navigator.clipboard.writeText(previousRecording.textContent).then(function() {
+ console.log('Previous recording copied to clipboard');
+ }, function(err) {
+ console.error('Failed to copy: ', err);
+ });
+})
+}
+
+
+
+document.getElementById('startBtn').addEventListener('click', function() {
+ var icon1 = document.getElementById('d1');
+ var icon2 = document.getElementById('d2');
+
+ icon1.classList.toggle('hidden');
+ icon2.classList.toggle('hidden');
+});
+
+
